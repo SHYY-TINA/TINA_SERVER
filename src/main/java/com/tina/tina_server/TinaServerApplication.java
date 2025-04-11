0fad5a9f5642eb0a6b4ad8daa34da34a4871b066
한태영 <s46422574@gmail.com>
2025-04-12 02:34:30 +0900

    set(#0): 파일 구조 세팅

---
+++ src/main/java/com/tina/tina_server/TinaServerApplication.java
@@ -0,0 +1,13 @@
+package com.tina.tina_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tina/tina_server/TinaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tina.tina_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
