23a710fcf5a3fb8e5bc3f41f2122b9b3ca8ff5b1
한태영 <s46422574@gmail.com>
2025-04-17 14:40:38 +0900

    feat: 회원 정보 조회 관련 DTO 구현

---
+++ src/main/java/com/tina/tina_server/domain/user/presentation/dto/res/BasicInfoResponse.java
@@ -0,0 +1,7 @@
+package com.tina.tina_server.domain.user.presentation.dto.res;
+
+public record BasicInfoResponse(
+        String nickname,
+        String mbti
+) {
+}
---
+++ src/main/java/com/tina/tina_server/domain/user/presentation/dto/res/ProfileImageUrlResponse.java
@@ -0,0 +1,6 @@
+package com.tina.tina_server.domain.user.presentation.dto.res;
+
+public record ProfileImageUrlResponse(
+        String profileImageUrl
+) {
+}
