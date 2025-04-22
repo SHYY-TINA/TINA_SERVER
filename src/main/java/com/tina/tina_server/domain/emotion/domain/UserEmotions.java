62cdb67f1975104f229bcd66d5e4a4a127bc8b05
한태영 <s46422574@gmail.com>
2025-04-22 10:36:25 +0900

    feat: UserEmotion 엔티티 구현

---
+++ src/main/java/com/tina/tina_server/domain/emotion/domain/UserEmotions.java
@@ -0,0 +1,39 @@
+package com.tina.tina_server.domain.emotion.domain;
+
+import com.tina.tina_server.domain.user.domain.Users;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEmotions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Users user;
+
+    private String partnerName;
+
+    private String partnerMbti;
+
+    private Integer charmScore;
+
+    private String feedbackTitle;
+
+    @Column(columnDefinition = "TEXT")
+    private String feedbackContent;
+
+    private String charmPointTitle;
+
+    @Column(columnDefinition = "TEXT")
+    private String charmPointContent;
+}
---
+++ src/main/java/com/tina/tina_server/domain/emotion/domain/repository/UserEmotionsRepository.java
@@ -0,0 +1,16 @@
+package com.tina.tina_server.domain.emotion.domain.repository;
+
+import com.tina.tina_server.domain.emotion.domain.ObservedEmotions;
+import com.tina.tina_server.domain.emotion.domain.UserEmotions;
+import com.tina.tina_server.domain.user.domain.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserEmotionsRepository extends JpaRepository<UserEmotions, Long> {
+
+    Optional<UserEmotions> findByIdAndUser(Long id, Users user);
+
+    List<UserEmotions> findAllByUser(Users user);
+}
