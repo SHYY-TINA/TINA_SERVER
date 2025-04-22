8bf6145027714b49eb107049643fe30dcefb4b2f
한태영 <s46422574@gmail.com>
2025-04-22 10:39:38 +0900

    feat: ObservedEmotions 엔티티 구현

---
+++ src/main/java/com/tina/tina_server/domain/mbti/domain/repository/MbtiCompatibilityRepository.java
@@ -0,0 +1,9 @@
+package com.tina.tina_server.domain.mbti.domain.repository;
+
+import com.tina.tina_server.domain.mbti.domain.MbtiCompatibility;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+public interface MbtiCompatibilityRepository extends JpaRepository<MbtiCompatibility, Long> {
+    Optional<MbtiCompatibility> findByFirstMbtiAndSecondMbti(String firstMbti, String secondMbti);
+}
