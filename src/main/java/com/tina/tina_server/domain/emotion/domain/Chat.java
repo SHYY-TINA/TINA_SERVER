5456ba4810b61586b281f111e21507434e409356
한태영 <s46422574@gmail.com>
2025-04-28 02:49:41 +0900

    feat: Chat Entity 수정

--- src/main/java/com/tina/tina_server/domain/emotion/domain/Chat.java
+++ src/main/java/com/tina/tina_server/domain/emotion/domain/Chat.java
@@ -15,6 +15,8 @@ public class Chat {
 
     private Long emotionId;
 
+    private String type;
+
     private String chat;
 
     private String meaning;
--- src/main/java/com/tina/tina_server/domain/emotion/domain/repository/ChatRepository.java
+++ src/main/java/com/tina/tina_server/domain/emotion/domain/repository/ChatRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-    List<Chat> findAllByEmotionId(Long emotionId);
+    List<Chat> findAllByEmotionIdAndType(Long emotionId, String type);
 
 }
