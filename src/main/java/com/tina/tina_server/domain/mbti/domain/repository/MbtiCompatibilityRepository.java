c3853167fa204beb99d65d80a95b195393416dda
한태영 <s46422574@gmail.com>
2025-05-15 22:47:17 +0900

    refactor: mbti 조회방식 변경

--- src/main/java/com/tina/tina_server/domain/mbti/domain/repository/MbtiCompatibilityRepository.java
+++ src/main/java/com/tina/tina_server/domain/mbti/domain/repository/MbtiCompatibilityRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 public interface MbtiCompatibilityRepository extends JpaRepository<MbtiCompatibility, Long> {
-    Optional<MbtiCompatibility> findByFirstMbtiAndSecondMbti(String firstMbti, String secondMbti);
+    Optional<MbtiCompatibility> findByFirstMbtiAndSecondMbtiOrSecondMbtiAndFirstMbti(
+            String firstMbti1, String secondMbti1,
+            String secondMbti2, String firstMbti2
+    );
 }
--- src/main/java/com/tina/tina_server/domain/mbti/service/implementation/MbtiReader.java
+++ src/main/java/com/tina/tina_server/domain/mbti/service/implementation/MbtiReader.java
@@ -13,11 +13,9 @@ public class MbtiReader {
     private final MbtiCompatibilityRepository mbtiCompatibilityRepository;
 
     public MbtiCompatibilityResponse findCompatibility(String firstMbti, String secondMbti) {
-        String sortedMbti1 = firstMbti.compareTo(secondMbti) <= 0 ? firstMbti : secondMbti;
-        String sortedMbti2 = firstMbti.compareTo(secondMbti) <= 0 ? secondMbti : firstMbti;
-
-        MbtiCompatibility data = mbtiCompatibilityRepository.findByFirstMbtiAndSecondMbti(sortedMbti1, sortedMbti2)
-                .orElseThrow(() -> new MbtiCompatibilityNotFoundException());
+        MbtiCompatibility data = mbtiCompatibilityRepository
+                .findByFirstMbtiAndSecondMbtiOrSecondMbtiAndFirstMbti(firstMbti, secondMbti, firstMbti, secondMbti)
+                .orElseThrow(MbtiCompatibilityNotFoundException::new);
 
         return new MbtiCompatibilityResponse(
                 data.getTitle(),
